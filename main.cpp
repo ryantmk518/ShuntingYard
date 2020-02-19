2da2607b1e572c5223efac1ad9f115092d7d5c9b
U-LAPTOP-UO9UPAVU\ryant <ryantmk518@gmail.com>
2020-02-18 19:42:32 -0800

    Transfer

--- main.cpp
+++ main.cpp
@@ -1,40 +1,83 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 #include "node.h"
 
 using namespace std;
 void push(Node* first, char* value);
 Node* queueHead = NULL;
-void pop(Node* first);
+Node* stackHead = NULL;
+void pop(Node* first, vector<char*> output);
 void enqueue(Node* first, char* value);
 void dequeue(Node* first);
 
 int main(){
-  bool oper = false;
-  bool num = false;
+  cout << "Put spaces between the numbers, operators, and parantheses" << endl;
   char input[99];
-  char temp[99];
+  int prio = 0;
+  vector<char*> output;
   cout << "Enter input" << endl;
   cin.get(input, 99);
   int a = 0;
-  for (int i = 0; i < strlen(input); i++) {
-    if (input[i] != ' ') {
-      char temp[a];
-      temp[a] = input[i];
+  while (a < strlen(input)){
+    if (input[a] == '+' || input[a] == '-' || input[a] == '*' || input[a] == '/' || input[a] == '^'){
+      if (input[a] == '+') {
+        prio = 2;
+      } 
+      else if (input[a] == '-') {
+        prio = 2;
+      }
+      else if (input[a] == '*') {
+        prio = 3;
+      }
+      else if (input[a] == '/') {
+        prio = 3;
+      }
+      else if (input[a] == '^') {
+        prio = 4;
+      }
+
+      char temp[1] = "";
+      temp[0]=input[a];
+      push(stackHead, temp);
       ++a;
     }
-    else if (input[i] == ' ') {
-      a = 0;
+    else if (input[a] == '(') {
+      char temp[2] = "(";
+      push(stackHead, temp);
+    }
+    else if (input[a] == ')') {
+      char temp[2] = ")";
+      push(stackHead, temp);
+    }
+    else if (input[a] == ' '){
+      ++a;
+    }
+    else {
+      char temp[9] = "";
+      int b = a;
+      int c = 0;
+      while (input[b] == '0' || input[b] == '1' || input[b] == '2' || input[b] == '3' || input[b] == '4' || input[b] == '5' || input[b] == '6' || input[b] == '7' || input[b] == '8' || input[b] == '9') {
+        temp[c] = input[b];
+        ++b;
+        ++c;
+      }
+      a = a + c;
+      temp[c] = '\n';
+      cout << temp << endl;
       push(queueHead, temp);
     }
   }
-  push(queueHead, temp);
 }
+
+
+
+
 
 void push(Node* first, char* value){ //Push function to add to the linked list
   Node* current = first;
   if (current == NULL){
-    cout << "Check" << endl;
+    cout << value << endl;
     current = new Node(NULL);
     current -> setValue(value);
   }
@@ -65,10 +108,11 @@ void enqueue(Node* first, char* value){ //Push function to add to the linked lis
   }
 }
 
-void pop(Node* first) {
+void pop(Node* first, vector<char*> output) {
   if (first->getNext()->getNext() == NULL) {
     if (first->getNext() == NULL) {
       cout << first -> getValue();
+      output.push_back(first -> getValue());
       first -> ~Node();
     }
     else {
@@ -78,7 +122,7 @@ void pop(Node* first) {
     }
   }
   else {
-    pop(first->getNext());
+    pop(first->getNext(), output);
   }
 }
 
